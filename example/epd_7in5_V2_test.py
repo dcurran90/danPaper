55235d3a13df42e67d98ed5fb358541a5a5d4356
Dan Curran <danielcurrn90@gmail.com>
2024-01-18 15:59:55 -0600

    starting to edit

--- example/epd_7in5_V2_test.py
+++ example/epd_7in5_V2_test.py
@@ -26,6 +26,14 @@ try:
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
 
+
+    Himage = Image.open(os.path.join(picdir, 'ink_border3.jpeg'))
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(4)
+
+
+
+    '''
     logging.info("read bmp file")
     Himage = Image.open(os.path.join(picdir, '7in5_V2.bmp'))
     epd.display(epd.getbuffer(Himage))
@@ -45,7 +53,6 @@ try:
     draw = ImageDraw.Draw(Himage)
     draw.text((10, 0), 'hello world', font = font24, fill = 0)
     draw.text((10, 20), '7.5inch e-Paper', font = font24, fill = 0)
-    # draw.text((150, 0), u'微雪电子', font = font24, fill = 0)
     draw.line((20, 50, 70, 100), fill = 0)
     draw.line((70, 50, 20, 100), fill = 0)
     draw.rectangle((20, 50, 70, 100), outline = 0)
@@ -71,28 +78,9 @@ try:
         if(num == 10):
             break
 
+    '''
 
 
-    # # Drawing on the Vertical image
-    # logging.info("2.Drawing on the Vertical image...")
-    # epd.init()
-    # Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    # draw = ImageDraw.Draw(Limage)
-    # draw.text((2, 0), 'hello world', font = font18, fill = 0)
-    # draw.text((2, 20), '7.5inch epd', font = font18, fill = 0)
-    # draw.text((20, 50), u'微雪电子', font = font18, fill = 0)
-    # draw.line((10, 90, 60, 140), fill = 0)
-    # draw.line((60, 90, 10, 140), fill = 0)
-    # draw.rectangle((10, 90, 60, 140), outline = 0)
-    # draw.line((95, 90, 95, 140), fill = 0)
-    # draw.line((70, 115, 120, 115), fill = 0)
-    # draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    # draw.rectangle((10, 150, 60, 200), fill = 0)
-    # draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    # epd.display(epd.getbuffer(Limage))
-    # time.sleep(2)
-
-    
 
     logging.info("Clear...")
     epd.init()
